4f8a5c475156c8c5f3cba2b99708e4913a899107
Mark Pentler <27921983+mpentler@users.noreply.github.com>
2023-05-07 22:04:32 +0100

    Update configure.sh
    Changed to match MLAT results port, should have gone in last commit.

--- configure.sh
+++ configure.sh
@@ -139,6 +139,6 @@ INPUT_TYPE="$INPUT_TYPE"
 
 MLATSERVER="feed.adsb.one:31090"
 TARGET="--net-connector feed.adsb.one,64004,beast_reduce_plus_out,feed.adsb.one,64005"
-NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30169 --net-bo-port 0 --net-ri-port 0 --write-json-every 1 --uuid-file /usr/local/share/adsb-one/adsb-one-uuid"
+NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 31094 --net-bo-port 0 --net-ri-port 0 --write-json-every 1 --uuid-file /usr/local/share/adsb-one/adsb-one-uuid"
 JSON_OPTIONS="--max-range 450 --json-location-accuracy 2 --range-outline-hours 24"
 EOF
